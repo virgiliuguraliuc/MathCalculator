1857bf431de4d7ba3d9a5a12b439a16935594dfe
virgiliu <virgiliuguraliuc2@gmail.com>
2019-07-16 14:18:44 +0300

    rewirite : my method allready accepted Double, made it Int, then override the methods for double in MathFunctionsExtended

--- src/main/java/org/fasttrackit/MathCalc.java
+++ src/main/java/org/fasttrackit/MathCalc.java
@@ -9,47 +9,55 @@ public class MathCalc {
     public static void main(String[] args) {
 
 
-      double A, B;
-
-        Scanner s = new Scanner(System.in);
-
-        System.out.println(" Type two numbers you want to do mathematical calculations with. ");
-        System.out.println(" Type the first number (A). ");
-        A = s.nextDouble();
-        System.out.println(" First number is: " + A);
-        System.out.println(" Type the second number (B). ");
-        B = s.nextDouble();
-        System.out.println(" Second number is: " + B);
-
-
       System.out.println("Select type of arithmetical operation:\n" +
               "1 - for adding\n" +
               "2 - for subtraction\n" +
               "3 - for multiplication\n" +
               "4 - for division\n");
 
-      MathFunctions calc = new MathFunctions();
+      MathFunctions calcInt = new MathFunctions();
+      MathFunctionsExtended calcDouble = new MathFunctionsExtended();
+
+//      Scanner s1 = new Scanner(System.in);
+//      int d = s1.nextInt();
+//
+//      if (d == 1 ) {
+//        calc.Adding();
+//      }
+//      if (d == 2 ) {
+//      calc.Subtract();
+//      }
+//      if (d == 3){
+//      calc.Multiply();
+//      }
+//      if (d == 4){
+//      calc.Divide();
+//      }
+//      else {
+//        System.out.println("Ai facut ceva ce nu ma asteptam, m-am panicat");
+//      }
 
 
       Scanner s1 = new Scanner(System.in);
       int d = s1.nextInt();
-      
+
       if (d == 1 ) {
-        calc.Adding(A,B);
+        calcDouble.Adding();
       }
       if (d == 2 ) {
-        calc.Subtract(A,B);
+        calcDouble.Subtract();
       }
       if (d == 3){
-        calc.Multiply(A,B);
+        calcDouble.Multiply();
       }
       if (d == 4){
-        calc.Divide(A,B);
+        calcDouble.Divide();
       }
       else {
         System.out.println("Ai facut ceva ce nu ma asteptam, m-am panicat");
       }
-
-
     }
+
+
+
 }
--- src/main/java/org/fasttrackit/MathFunctionsExtended.java
+++ src/main/java/org/fasttrackit/MathFunctionsExtended.java
@@ -1,76 +1,49 @@
 package org.fasttrackit;
 
-import java.util.Scanner;
+public class MathFunctionsExtended extends MathFunctions {
 
-public class MathFunctions {
-
-
+    @Override
     public void Adding() {
         System.out.println(" Type two numbers you want to do mathematical calculations with \n Example A+B ");
         System.out.println("type first number A:");
-        int a = Scanner();
+        double a = ScannerD();
         System.out.println("type second numberB :");
-        int b = Scanner();
-        int c = (a + b);
+        double b = ScannerD();
+        double c = (a + b);
         System.out.println(" You have selected Add, adding (A + B) result is: " + c);
 
     }
 
+    @Override
     public void Subtract() {
         System.out.println(" Type two numbers you want to do mathematical calculations with \n Example A-B ");
         System.out.println("type first number A:");
-        int a = Scanner();
+        double a = ScannerD();
         System.out.println("type second numberB :");
-        int b = Scanner();
-        int c = (a - b);
+        double b = ScannerD();
+        double c = (a - b);
         System.out.println(" You have selected Subtract, subtracting (A - B) result is: " + c);
-
     }
 
+    @Override
     public void Multiply() {
         System.out.println(" Type two numbers you want to do mathematical calculations with \n Example A*B ");
         System.out.println("type first number A:");
-        int a = Scanner();
+        double a = ScannerD();
         System.out.println("type second numberB :");
-        int b = Scanner();
-        int c = (a * b);
+        double b = ScannerD();
+        double c = (a * b);
         System.out.println(" You have selected Multiply, multiplying (A * B) result is: " + c);
-
     }
 
+    @Override
     public void Divide() {
         System.out.println(" Type two numbers you want to do mathematical calculations with \n Example A/B ");
         System.out.println("type first number A:");
-        int a = Scanner();
+        double a = ScannerD();
         System.out.println("type second numberB :");
-        int b = Scanner();
-        int c = (a / b);
+        double b = ScannerD();
+        double c = (a / b);
         System.out.println(" You have selected Divide, dividing (A / B), result is: " + c);
-
-    }
-
-
-    public int Scanner(){
-
-
-        Scanner s = new Scanner(System.in);
-        int input ;
-        System.out.println(" Type the number: ");
-        input = s.nextInt();
-
-
-        return input;
-    }
-
-
-    public double ScannerD(){
-
-        Scanner s = new Scanner(System.in);
-        double inputD ;
-        System.out.println(" Type the number: ");
-        inputD = s.nextDouble();
-
-
-        return inputD;
     }
 }
